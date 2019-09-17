22d1993225c542076bc9a89ae7668b32217daf11
Ben <8bhsolutions@gmail.com>
2019-09-17 22:39:51 +1000

    change route links overwrite back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,68 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav, NavController} from 'ionic-angular';
 import { Deeplinks } from '@ionic-native/deeplinks';
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
-import { TabsPage } from '../pages/tabs/tabs';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { App, Nav, Platform, ToastController } from 'ionic-angular';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
-  template: `<ion-nav [root]="rootPage"></ion-nav>`
+	template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage = TabsPage;
-  
-  @ViewChild(Nav) navChild:Nav;
+	rootPage = TabsPage;
 
-  constructor(
-    platform: Platform,
-    public statusBar: StatusBar,
-    public splashScreen: SplashScreen,
-    public deeplinks: Deeplinks
-  ) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+	@ViewChild(Nav) navChild: Nav;
 
-      //this is the code who responds to the app deeplinks
-      //Deeplinks if from Ionic Native
-      this.deeplinks.routeWithNavController(this.navChild, {
-        '/about': AboutPage,
-        '/contact': ContactPage,
-        '/items/:itemId': ItemDetailsPage
-      }).subscribe((match) => {
-        console.log('Successfully routed', match);
-      }, (nomatch) => {
-        console.log('Unmatched Route', nomatch);
-      });
-    });
-  }
+	constructor(
+		private platform: Platform,
+		private app: App,
+		private toast: ToastController,
+		public statusBar: StatusBar,
+		public splashScreen: SplashScreen,
+		public deeplinks: Deeplinks
+	) {
+		platform.ready()
+			.then(() => {
+				// Okay, so the platform is ready and our plugins are available.
+				// Here you can do any higher level native things you might need.
+				this.statusBar.styleDefault();
+
+				//this is the code who responds to the app deeplinks
+				//Deeplinks if from Ionic Native
+				this.deeplinks.routeWithNavController(this.navChild, {
+					'/L/about': AboutPage,
+					'/L/contact': ContactPage,
+					'/L/items/:itemId': ItemDetailsPage
+				}).subscribe((match) => {
+					console.log('Successfully routed', match);
+				}, (nomatch) => {
+					console.log('Unmatched Route', nomatch);
+				});
+			})
+			.then(() => {
+				this.overrideBackButton();
+			});
+	}
+
+	private overrideBackButton() {
+		this.platform.registerBackButtonAction(() => {
+			let nav = this.app.getActiveNavs()[0];
+			let activeView: any = nav.getActive();
+
+			if (activeView != null) {
+				if (nav.canGoBack()) {
+					nav.pop();
+				} else {
+					this.toast.create({
+						message: 'On root page',
+						duration: 3000,
+						showCloseButton: true
+					});
+				}
+			}
+		});
+	}
 }
